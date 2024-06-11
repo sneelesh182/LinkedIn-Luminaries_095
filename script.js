a16c30958f048a66bc43c5d26f8c0d886a262632
Kranti Kumar <krantikumar@Krantis-MacBook-Air.local>
2024-06-11 21:50:18 +0530

    Git repo created

---
+++ script.js
@@ -0,0 +1,27 @@
+// Avani Part Start
+
+// Avani Part End
+
+
+
+// Shasikant Part Start
+
+//  Shasikant Part End
+
+
+
+// Neelesh Part Start
+
+// Neelesh Part End
+
+
+
+// Ayush Part Start
+
+// Ayush Part End
+
+
+
+// Kranti Part Start
+
+// Kranti Part End
